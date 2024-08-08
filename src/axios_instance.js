0b5f5a2faa5c14f43e470b567ebf920c87f3c1d4
deva kumar singh <imdeva91@gmail.com>
2024-08-08 21:15:57 +0530

    add

--- src/axios_instance.js
+++ src/axios_instance.js
@@ -2,12 +2,14 @@ import axios from "axios";
 
 const Axiosinstance = axios.create({
   baseURL : 'https://onlinecompiler-backend.onrender.com',
-  headers: {
- Authorization: `
- Bearer ${localStorage.getItem('token')}
- `,
-  }, 
+ 
   // .. other options
 });
+Axiosinstance.interceptors.request.use(async config => {
+ 
+
+  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}` // you may need "Bearer" here
+  return config
+})
 
 export default Axiosinstance;
